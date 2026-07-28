31028de851f98d5d6e643f3222bb50afa74c03fa
Кожухарев Дмитрий <dima.kozhuharew@yandex.ru>
2026-07-28 23:50:24 +0500

    feat: clean->package файл правильно запускается и отображает, что нужно `java -jar "$INST_DIR\InstanceUpdater.jar" "$INST_ID"`

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,27 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("=== InstanceUpdater ===");
+
+        if (args.length == 0) {
+
+            System.err.println("Pack name not specified.");
+
+            System.exit(1);
         }
+
+        String pack = args[0];
+
+        Path instance = Paths.get("").toAbsolutePath();
+
+        System.out.println("Pack      : " + pack);
+        System.out.println("Instance  : " + instance);
+
     }
+
 }
